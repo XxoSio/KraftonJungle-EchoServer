5bd50d05572784ea079e21c1aae3462ad16b5699
XxoSio <ghdtjgml708@naver.com>
2022-12-09 12:43:06 +0900

    echo_function comment

--- echo_function.c
+++ echo_function.c
@@ -6,9 +6,14 @@ void echo(int connfd)
 	char buf[MAXLINE];
 	rio_t rio;
 
+	// open한 식별자마다 한번 호출
+	// 식별자 connfd를 주소 rio에 위치한 읽기 버퍼와 연결
 	Rio_readinitb(&rio, connfd);
+
+	// rio에서 얻은 값을 buf에 복사
 	while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
 		printf("server received %d bytes\n", (int)n);
+		// buf의 값을 connfd에 복사
 		Rio_writen(connfd, buf, n);
 	}
 }
